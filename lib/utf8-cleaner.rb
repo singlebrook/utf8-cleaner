0177a8aacd88c22c09e2d145828e7ddec1242f45
Leon Miller-Out <leon@singlebrook.com>
2013-04-22 14:06:12 -0400

    Initial bundler gem build

---
+++ lib/utf8-cleaner.rb
@@ -0,0 +1,7 @@
+require "utf8-cleaner/version"
+
+module Utf8
+  module Cleaner
+    # Your code goes here...
+  end
+end
---
+++ lib/utf8-cleaner/version.rb
@@ -0,0 +1,5 @@
+module Utf8
+  module Cleaner
+    VERSION = "0.0.1"
+  end
+end

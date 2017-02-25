7176886b9105d9d2f19a921afb557816abfecb9b
Thomas Brooks <d3vkit@gmail.com>
2017-02-24 17:18:30 -0800

    Add Methods For Clean or Raise
    Allow app to specify args to middleware to clean or raise

---
+++ lib/utf8-cleaner/methods/clean_method.rb
@@ -0,0 +1,63 @@
+module UTF8Cleaner
+  module Methods
+    class CleanMethod
+      SANITIZE_ENV_KEYS = %w(
+        HTTP_REFERER
+        HTTP_USER_AGENT
+        PATH_INFO
+        QUERY_STRING
+        REQUEST_PATH
+        REQUEST_URI
+        HTTP_COOKIE
+      ).freeze
+
+      include ActiveSupport::Multibyte::Unicode
+
+      def initialize(env)
+        @env = env
+      end
+
+      def sanitize_env
+        sanitize_env_keys
+        sanitize_env_rack_input
+
+        @env
+      end
+
+      def sanitize_env_keys
+        SANITIZE_ENV_KEYS.each do |key|
+          next unless value = @env[key]
+          @env[key] = cleaned_string(value)
+        end
+      end
+
+      def sanitize_env_rack_input
+        case @env['CONTENT_TYPE']
+        when 'application/x-www-form-urlencoded'
+          # This data gets the full cleaning treatment
+          cleaned_value = cleaned_string(@env['rack.input'].read)
+          @env['rack.input'] = StringIO.new(cleaned_value) if cleaned_value
+          @env['rack.input'].rewind
+        when 'application/json'
+          # This data only gets cleaning of invalid UTF-8 (e.g. from another charset)
+          # but we do not URI-decode it.
+          rack_input = @env['rack.input'].read
+          if rack_input && !rack_input.ascii_only?
+            @env['rack.input'] = StringIO.new(tidy_bytes(rack_input))
+          end
+          @env['rack.input'].rewind
+        when 'multipart/form-data'
+          # Don't process the data since it may contain binary content
+        else
+          # Unknown content type. Leave it alone
+        end
+      end
+
+      def cleaned_string(value)
+        value = tidy_bytes(value) unless value.ascii_only?
+        value = URIString.new(value).cleaned if value.include?('%')
+        value
+      end
+    end
+  end
+end
--- spec/middleware_spec.rb
+++ spec/middleware_spec.rb
@@ -3,104 +3,40 @@ require 'rack/lint'
 
 module UTF8Cleaner
   describe Middleware do
-    let :new_env do
-      Middleware.new(nil).send(:sanitize_env, env)
-    end
-
-    describe "with a big nasty env" do
-      let :env do
-        {
-          'PATH_INFO' => 'foo/%FFbar%2e%2fbaz%26%3B',
-          'QUERY_STRING' => 'foo=bar%FF',
-          'HTTP_REFERER' => 'http://example.com/blog+Result:+%ED%E5+%ED%E0%F8%EB%EE%F1%FC+%F4%EE%F0%EC%FB+%E4%EB%FF+%EE%F2%EF%F0%E0%E2%EA%E8',
-          'HTTP_USER_AGENT' => "Android Versi\xF3n/4.0\x93",
-          'REQUEST_URI' => '%C3%89%E2%9C%93',
-          'rack.input' => StringIO.new("foo=%FFbar%F8"),
-          'CONTENT_TYPE' => 'application/x-www-form-urlencoded',
-          'HTTP_COOKIE' => nil
-        }
-      end
-
-      describe "removes invalid %-encoded UTF-8 sequences" do
-        it { expect(new_env['QUERY_STRING']).to eq('foo=bar') }
-        it { expect(new_env['HTTP_REFERER']).to eq('http://example.com/blog+Result:+++++') }
-        it { expect(new_env['rack.input'].read).to eq('foo=bar') }
-      end
-
-      describe 'replaces \x-encoded characters from the ISO-8859-1 and CP1252 code pages with their UTF-8 equivalents' do
-        it { expect(new_env['HTTP_USER_AGENT']).to eq("Android Versi\u00F3n/4.0\u201C") }
-      end
-
-      describe "leaves all valid characters untouched" do
-        it { expect(new_env['PATH_INFO']).to eq('foo/bar%2e%2fbaz%26%3B') }
-        it { expect(new_env['REQUEST_URI']).to eq('%C3%89%E2%9C%93') }
-      end
-
-      describe "when rack.input is wrapped" do
-        # rack.input responds only to methods gets, each, rewind, read and close
-        # Rack::Lint::InputWrapper is the class which servers wrappers are based on
-        it "removes invalid UTF-8 sequences" do
-          wrapped_rack_input = Rack::Lint::InputWrapper.new(StringIO.new("foo=%FFbar%F8"))
-          env.merge!('rack.input' => wrapped_rack_input)
-          new_env = Middleware.new(nil).send(:sanitize_env, env)
-          expect(new_env['rack.input'].read).to eq('foo=bar')
-        end
-      end
-
-      describe "when binary data is POSTed" do
-        before do
-          env['CONTENT_TYPE'] = 'multipart/form-data'
-        end
-        it "leaves the body alone" do
-          env['rack.input'].rewind
-          expect(new_env['rack.input'].read).to eq "foo=%FFbar%F8"
-        end
-      end
-
-      describe "when json data is POSTed" do
-        before do
-          env['CONTENT_TYPE'] = 'application/json'
-        end
-
-        it "tidies invalid UTF-8 sequences" do
-          env['rack.input'] = StringIO.new(%Q({"foo": "\xFFbar\xF8"}))
-          env['rack.input'].rewind
-          expect(new_env['rack.input'].read).to eq(%Q({"foo": "\u00FFbar\u00F8"}))
-        end
-
-        it "does not attempt to URI-decode data" do
-          json = %Q({"foo": "%FF"})
-          env['rack.input'] = StringIO.new(json)
-          env['rack.input'].rewind
-          expect(new_env['rack.input'].read).to eq(json)
-        end
+    context 'with invalid method' do
+      it 'raises ArgumentError' do
+        expect { Middleware.new(nil, :invalid_method) }.to raise_error(ArgumentError)
       end
     end
 
-    describe "with a minimal env" do
+    context 'with clean method' do
       let(:env) do
-        {
-          'PATH_INFO' => '/this/is/safe',
-          'QUERY_STRING' => 'foo=bar%FF'
-        }
+        { 'PATH_INFO' => '/this/is/safe', 'QUERY_STRING' => 'foo=bar%FF' }
       end
 
-      it "only runs URIString cleaning on potentially unclean strings" do
-        expect(URIString).to receive(:new).once.and_call_original
-        new_env
+      let(:new_env) do
+        { 'PATH_INFO' => '/this/is/cleaned', 'QUERY_STRING' => 'bar=foo%FF' }
       end
 
-      it "leaves clean values alone" do
-        expect(new_env['PATH_INFO']).to eq('/this/is/safe')
-      end
-    end
+      it 'uses CleanMethod on the env' do
+        app = double('app', call: [200, {}, []])
+        clean_method = instance_double(Methods::CleanMethod, sanitize_env: new_env)
+        allow(Methods::CleanMethod).to receive(:new).with(env) { clean_method }
 
-    # Ensure that all cleaned values parse cleanly.
-    # E.g. make sure Rack/Rails won't choke on them
-    after do
-      cleaned = new_env
-      env.keys.reject{|key| key == 'rack.input'}.each do |key|
-        URI.decode_www_form_component(cleaned[key]) if cleaned[key]
+        Middleware.new(app, :clean).call(env)
+
+        expect(Methods::CleanMethod).to have_received(:new).with(env)
+      end
+
+      it 'passes the sanitized env to the app' do
+        app = double('app')
+        clean_method = instance_double(Methods::CleanMethod, sanitize_env: new_env)
+        allow(Methods::CleanMethod).to receive(:new).with(env) { clean_method }
+        allow(app).to receive(:call).with(new_env)
+
+        Middleware.new(app, :clean).call(env)
+
+        expect(app).to have_received(:call).with(new_env)
       end
     end
   end
